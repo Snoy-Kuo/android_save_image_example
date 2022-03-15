c9a31cb1e7dab9bee058eac5b5ee62f9220eff53
Snoy Kuo <snoykuo@gmail.com>
2022-03-15 10:48:22 +0800

    create project by AS fragment+viewmodel template

---
+++ app/src/main/java/com/snoy/save_img_example/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.snoy.save_img_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.snoy.save_img_example.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/snoy/save_img_example/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.snoy.save_img_example.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
